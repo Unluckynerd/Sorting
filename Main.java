b79cdb82dcf9686794b85cf09c640c8118ed02e0
Muthu <95872423+Unluckynerd@users.noreply.github.com>
2022-03-28 21:30:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 package sort;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int a[] = { 3,7,98,12,4,1};
-        int b[] = {34,-1,5,23,99,8};
-        test Arrays = new test();
-        Arrays.sort(new BubbleSort(),a);
-        Arrays.sort(new InsertionSort(),b);
-        System.out.println(java.util.Arrays.toString(a));
-        System.out.println(java.util.Arrays.toString(b));
+        array obj = new Bubblesort();
+        array obj1 = new InsertionSort();
+        Test Binary = new Test();
+        int a[] = {3, 7, 98, 12, 4, 1};
+        int b[] = {34, -1, 5, 23, 99, 8};
+        System.out.println(Binary.search(obj, a, 5));
+        System.out.println(Binary.search(obj1, b, 5));
     }
 }
