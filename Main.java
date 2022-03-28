6c115b27a78a480065031727d735c1dce04a878a
Muthu <95872423+Unluckynerd@users.noreply.github.com>
2022-03-28 09:48:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ package Sorting;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        BubbleSort b = new BubbleSort();
-        InsertionSort i = new InsertionSort();
-        int a[] = {4,9,2,12,6,3,1};
-        b.sort(a);
+    public static void main(String[] args) throws Exception {
+        int a[] = {4, 9, 2, 12, 6, 3, 1};
+        Array obj= new BubbleSort();
+        obj.sort(a);
         System.out.println(Arrays.toString(a));
-        int c[] = {14,34,63,1,13,4};
-        i.sort(c);
-        System.out.println(Arrays.toString(c));
+        int b[] = {5,32,-1,64,2,10};
+        Array obj2 = new InsertionSort();
+        obj2.sort(b);
+        System.out.println(Arrays.toString(b));
     }
 }
