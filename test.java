c79c39f6963a808b48cf9556e1b0ad75dabab9b1
Muthu <95872423+Unluckynerd@users.noreply.github.com>
2022-03-28 12:50:03 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+package sort;
+
+public class test {
+
+    public void sort( Bubblesort obj,int a[])
+    {
+        obj.sort(a);
+    }
+
+    public  void sort(InsertionSort i , int a[])
+    {
+        i.sort(a);
+    }
+}
