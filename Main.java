ea08bfce525a97caf6048fe8d3276e0b4f8d1be3
Muthu <95872423+Unluckynerd@users.noreply.github.com>
2022-03-28 12:52:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         int a[] = { 3,7,98,12,4,1};
         int b[] = {34,-1,5,23,99,8};
         test Arrays = new test();
-        Arrays.sort(new Bubblesort(),a);
+        Arrays.sort(new BubbleSort(),a);
         Arrays.sort(new InsertionSort(),b);
         System.out.println(java.util.Arrays.toString(a));
         System.out.println(java.util.Arrays.toString(b));
